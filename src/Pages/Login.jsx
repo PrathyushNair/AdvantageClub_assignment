f2b413d0a425838983fcfab6715020ae40826e45
PrathyushNair <prathyu360@gmail.com>
2022-09-05 15:14:59 +0530

    final push

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,28 +3,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { loginaction, noerroraction } from "../Redux/loginaction";
 import { Spinner } from "@chakra-ui/react";
-import {
-  Alert,
-  AlertIcon,
-} from "@chakra-ui/react";
+import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 import style from "./Login.module.css";
 export const Login = () => {
   const dispatch = useDispatch();
   const { isAuth, isError, isLoading } = useSelector((state) => state.login);
   let [data, setData] = React.useState({ email: "", password: "" });
-  let [emptyField,setemptyField]=React.useState(false)
+  let [emptyField, setemptyField] = React.useState(false);
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
   let handleChange = (e) => {
-    dispatch(noerroraction())
-    setemptyField(false)
+    dispatch(noerroraction());
+    setemptyField(false);
     setData({ ...data, [e.target.name]: e.target.value });
   };
   let handleSubmit = () => {
-    if(data.email==""||data.password=="")
-    {
-      setemptyField(true)
-    }
-    else{
-
+    if (data.email === "" || data.password === "") {
+      setemptyField(true);
+    } else {
       dispatch(loginaction(data));
     }
   };
@@ -40,33 +37,40 @@ export const Login = () => {
           Please check login credentials
         </Alert>
       )}
-       {emptyField && (
+      {emptyField && (
         <Alert status="error">
           <AlertIcon />
-        Please fill all fields
+          Please fill all fields
         </Alert>
       )}
       <div>
-        <input
+        <Input
           name="email"
           value={data.email}
           onChange={handleChange}
           placeholder="Enter Email"
-        ></input>
+          size="md"
+        />
       </div>
       <div>
-        <input
-          name="password"
-          type="password"
-          value={data.password}
-          onChange={handleChange}
-          placeholder="Enter password"
-        ></input>
+        <InputGroup size="md">
+          <Input
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            pr="4.5rem"
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
       </div>
 
-      <button onClick={handleSubmit}>Submit</button>
+      <Button onClick={handleSubmit}>Submit</Button>
     </div>
   );
 };
-
-
